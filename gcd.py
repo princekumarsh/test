630aeba0b874ad9d4b093bfe9f8d01b94226bdc5
princekumarsh <princekumarsh@gmail.com>
2023-09-13 10:50:36 +0530

    This is the first version of gcd test and test1 file

---
+++ gcd.py
@@ -0,0 +1,13 @@
+num1=int(input("enter number 1: "))
+num2=int(input("enter number 2: "))
+def gcd(num1,num2):
+    if num1>num2:
+        small=num2
+    else:
+        small= num1
+    for i in range(1,small+1):
+        if num1%i==0 and num2%i==0:
+            gcd=i
+    lcm=(num1*num2)//gcd
+    print("lcm of {} and {} is = {} \ngcd of {} and {} is = {}".format(num1,num2,lcm,num1,num2,gcd))
+gcd(num1,num2)
